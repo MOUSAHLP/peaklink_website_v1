e166b81dc6691483d9545f7c2a83a47f49eda9e7
YacoubAlhaidari <Yacoub.haid@cloudszone.tech>
2024-01-26 22:33:03 +0300

    add .

---
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Pages;
+use Filament\Panel;
+use Filament\Widgets;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Http\Middleware\Authenticate;
+use Filament\SpatieLaravelTranslatablePlugin;
+use pxlrbt\FilamentSpotlight\SpotlightPlugin;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Z3d0X\FilamentFabricator\FilamentFabricatorPlugin;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+        ->plugins([
+            FilamentFabricatorPlugin::make(),
+            FilamentShieldPlugin::make()
+            ->gridColumns([
+                'default' => 1,
+                'sm' => 2,
+                'lg' => 3
+            ])
+            ->sectionColumnSpan(1)
+            ->checkboxListColumns([
+                'default' => 1,
+                'sm' => 2,
+                'lg' => 4,
+            ])
+            ->resourceCheckboxListColumns([
+                'default' => 1,
+                'sm' => 2,
+            ]),
+
+
+            \Awcodes\Curator\CuratorPlugin::make()
+                ->label('Media')
+                ->pluralLabel('Media')
+                ->navigationIcon('heroicon-o-photo')
+                ->navigationGroup('Content')
+                ->navigationSort(3)
+                ->navigationCountBadge(),
+
+                SpatieLaravelTranslatablePlugin::make()
+                ->defaultLocales(['en', 'ar']),
+
+                \Hasnayeen\Themes\ThemesPlugin::make(),
+
+                SpotlightPlugin::make(),
+
+
+
+
+
+            
+        ])
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+                \Hasnayeen\Themes\Http\Middleware\SetTheme::class
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ])
+            ->resources([
+                config('filament-logger.activity_resource')
+            ]);
+    }
+}
